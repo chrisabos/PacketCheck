21a2bdcaaa3db258e852465158b50cf7a540fd27
chrisabos <chrisalanbos@gmail.com>
2020-04-23 17:13:11 -0400

    First bugfix

--- packetcheck.py
+++ packetcheck.py
@@ -4,7 +4,7 @@ def check(packet):
 	hex = binascii.hexlify(packet)
 
 	print("Internet Protocol")
-	print("\tVersion: %d".format(hex[0]))
+	print("\tVersion: {}".format(hex[0]))
 
 if __name__ == "__main__":
 	print("This is not how this works")
