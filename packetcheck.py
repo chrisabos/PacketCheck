778011d80cf44c9f6dee564d69afb9c8d5ff4f57
chrisabos <chrisalanbos@gmail.com>
2020-04-23 17:34:41 -0400

    Now using struct.unpack to make life easy

--- packetcheck.py
+++ packetcheck.py
@@ -1,13 +1,25 @@
-import binascii
+import struct, binascii
 
 def check(packet):
-	print("Internet Protocol")
-	print("\tVER: {}".format(packet[0]))
-	print("\tIHL: {}".format(packet[0]))
-	print("\t-- TOS")
-	print("\t\tDSCP: {}".format(packet[1]>>2))
-	print("\t\tECN:  {}".format(packet[1]&0x03))
-	print("\tTotal Length: {}".format(packet[2:3]))
+	ip_ver = packet[0]>>4;
+
+	if ip_ver == 4:
+		fields = struct.unpack('BBHHHBBHLL')
+
+		print("Internet Protocol")
+		print("\tVer: {}".format(fields[0]>>4))
+		print("\tIHL: {}".format(fields[0]&0x0F))
+		print("\tDSCP: {}".format(fields[1]>>2))
+		print("\tECN: {}".format(fields[1]&0x03))
+		print("\tTotal Length: {}".format(fields[2:3]))
+
+	# print("Internet Protocol")
+	# print("\tVER: {}".format(packet[0]>>4))
+	# print("\tIHL: {}".format(packet[0]&0x0F))
+	# print("\t-- TOS")
+	# print("\t\tDSCP: {}".format(packet[1]>>2))
+	# print("\t\tECN:  {}".format(packet[1]&0x03))
+	# print("\tTotal Length: {}".format(packet[2:3]))
 
 if __name__ == "__main__":
 	print("This is not how this works")
