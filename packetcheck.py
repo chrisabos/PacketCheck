8d2313fdb76a88c057f90ddfed181d6e1a05cec2
chrisabos <chrisalanbos@gmail.com>
2020-04-23 17:20:01 -0400

    expanding prints

--- packetcheck.py
+++ packetcheck.py
@@ -2,9 +2,15 @@ import binascii
 
 def check(packet):
 	hex = binascii.hexlify(packet)
+	print(hex)
 
 	print("Internet Protocol")
-	print("\tVersion: {}".format(hex[0]))
+	print("\tVER: {}".format(hex[0]))
+	print("\tIHL: {}".format(hex[1]))
+	print("\t-- TOS")
+	print("\t\tDSCP: {}".format(hex[2:2]>>2))
+	print("\t\tECN:  {}".format(hex[2:2]&0x03))
+	print("\tTotal Length: {}".format(hex[4:4]))
 
 if __name__ == "__main__":
 	print("This is not how this works")
